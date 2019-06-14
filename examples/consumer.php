3639fb96e2e637a85b34dcfaccf0cacd87e41cca
Mohammed Amine El houba <amine.elhouba@gmail.com>
2019-06-14 17:55:16 +0200

    Forked from avro-php : tests with php-rdkafka

---
+++ examples/consumer.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once('../lib/avro.php');
+
+$rk = new RdKafka\Consumer();
+$rk->setLogLevel(LOG_DEBUG);
+$rk->addBrokers("127.0.0.1");
+
+$topic = $rk->newTopic("test_avro");
+
+$topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
+
+while (true) {
+
+    $msg = $topic->consume(0, 1000);
+    if ($msg) {
+        if ($msg->err) {
+            echo $msg->errstr(), "\n";
+            break;
+        } else {
+            // Load the string data string
+            $read_io = new AvroStringIO($msg->payload);
+            $data_reader = new AvroDataIOReader($read_io, new AvroIODatumReader());
+            echo "from binary string:\n";
+            foreach ($data_reader->data() as $datum)
+                echo var_export($datum, true) . "\n";
+        }
+    }
+}
